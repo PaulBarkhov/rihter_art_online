e91bd4356356f55893e2e85b9fd31c8d345be2f1
paulbarkhov <pavelbarhov@gmail.com>
2022-03-27 13:48:04 +0200

    refactored requests

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -13,67 +13,34 @@ import {
     DELETE_USER_FAIL
 } from './types'
 
-// export const checkAuthentication = () => async dispatch => {
-//     try {
-//         axios.get(`${process.env.REACT_APP_API_URL}/authentication/check_authentication`)
-//             .then(res => {
-//                 if (res.data.isAuthenticated) dispatch({
-//                     type: AUTHENTICATED_SUCCESS,
-//                     payload: true
-//                 })
-//                 else dispatch({
-//                     type: AUTHENTICATED_FAIL,
-//                     payload: false
-//                 })
-//             })
-//     } catch (error) {
-//         dispatch({
-//             type: AUTHENTICATED_FAIL,
-//             payload: false
-//         })
-//     }
-// }
-
 export const checkAuthentication = () => async dispatch => {
-    const config = {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    };
-
     try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/authentication/check_authentication`, config)
+        await axios.get(`${process.env.REACT_APP_API_URL}/authentication/check_authentication`)
+            .then(res => {
+                if (res.data.error || !res.data.isAuthenticated) dispatch({
+                    type: AUTHENTICATED_FAIL,
+                    payload: false
+                })
 
-        console.log(res)
-
-        if (res.data.error || !res.data.isAuthenticated) {
-            dispatch({
-                type: AUTHENTICATED_FAIL,
-                payload: false
-            });
-        }
-        else if (res.data.isAuthenticated) {
-            dispatch({
-                type: AUTHENTICATED_SUCCESS,
-                payload: true
-            });
-        }
-        else {
-            dispatch({
-                type: AUTHENTICATED_FAIL,
-                payload: false
-            });
-        }
+                else if (res.data.isAuthenticated) dispatch({
+                    type: AUTHENTICATED_SUCCESS,
+                    payload: true
+                })
+                else dispatch({
+                    type: AUTHENTICATED_FAIL,
+                    payload: false
+                })
+            })
     } catch (err) {
         dispatch({
             type: AUTHENTICATED_FAIL,
             payload: false
-        });
+        })
     }
-};
+}
 
 export const register = (userData) => async dispatch => {
+    const body = JSON.stringify(userData)
     const config = {
         headers: {
             'Accept': 'application/json',
@@ -81,29 +48,19 @@ export const register = (userData) => async dispatch => {
             'X-CSRFToken': Cookies.get('csrftoken')
         }
     }
-
-    const body = JSON.stringify(userData)
-
     try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/authentication/register`, body, config)
+        await axios.post(`${process.env.REACT_APP_API_URL}/authentication/register`, body, config)
+            .then(res => {
+                if (res.data.error) dispatch({ type: REGISTER_FAIL })
+                else dispatch({ type: REGISTER_SUCCESS })
+            })
 
-        if (res.data.error) {
-            dispatch({
-                type: REGISTER_FAIL
-            })
-        } else {
-            dispatch({
-                type: REGISTER_SUCCESS
-            })
-        }
-    } catch (err) {
-        dispatch({
-            type: REGISTER_FAIL
-        })
-    }
+
+    } catch (err) { dispatch({ type: REGISTER_FAIL }) }
 }
 
 export const login = (userData) => async dispatch => {
+    const body = JSON.stringify(userData)
     const config = {
         headers: {
             'Accept': 'application/json',
@@ -111,77 +68,30 @@ export const login = (userData) => async dispatch => {
             'X-CSRFToken': Cookies.get('csrftoken')
         }
     }
-
-    const body = JSON.stringify(userData)
-
     try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/authentication/login`, body, config)
-
-        if (res.data.success) {
-            dispatch({
-                type: LOGIN_FAIL
+        await axios.post(`${process.env.REACT_APP_API_URL}/authentication/login`, body, config)
+            .then(res => {
+                if (res.data.success) dispatch({ type: LOGIN_SUCCESS })
+                else dispatch({ type: LOGIN_FAIL })
             })
-        } else {
-            dispatch({
-                type: LOGIN_FAIL
-            })
-        }
-    } catch (err) {
-        dispatch({
-            type: LOGIN_FAIL
-        })
-    }
+    } catch (err) { dispatch({ type: LOGIN_FAIL }) }
 }
 
-// export const logout = () => async dispatch => {
-//     try {
-//         await axios.post(`${process.env.REACT_APP_API_URL}/authentication/logout`,
-//             {
-//                 headers: {
-//                     'Accept': 'application/json',
-//                     'Content-Type': 'application/json',
-//                     'X-CSRFToken': Cookies.get('csrftoken')
-//                 },
-//                 body: JSON.stringify({ 'withCredentials': true })
-//             })
-//             .then(res => {
-//                 if (res.data.success) dispatch({ type: LOGOUT_SUCCESS })
-//                 else dispatch({ type: LOGOUT_FAIL })
-//             })
-//     } catch (error) {
-//         dispatch({ type: LOGOUT_FAIL })
-//     }
-
-// }
-
 export const logout = () => async dispatch => {
+    const body = JSON.stringify({ 'withCredentials': true })
     const config = {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'X-CSRFToken': Cookies.get('csrftoken')
         }
-    };
-
-    const body = JSON.stringify({
-        'withCredentials': true
-    });
-
-    try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/authentication/logout`, body, config);
-
-        if (res.data.success) {
-            dispatch({
-                type: LOGOUT_SUCCESS
-            });
-        } else {
-            dispatch({
-                type: LOGOUT_FAIL
-            });
-        }
-    } catch (err) {
-        dispatch({
-            type: LOGOUT_FAIL
-        });
     }
-};
+    try {
+        await axios.post(`${process.env.REACT_APP_API_URL}/authentication/logout`, body, config)
+            .then(res => {
+                console.log(`logout res: ${res}`)
+                if (res.data.success) dispatch({ type: LOGOUT_SUCCESS })
+                else dispatch({ type: LOGOUT_FAIL })
+            })
+    } catch (err) { dispatch({ type: LOGOUT_FAIL }) }
+}
