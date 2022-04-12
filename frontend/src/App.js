d101f24a5230cb2a31044d4f45603ee23a723792
paulbarkhov <pavelbarhov@gmail.com>
2022-04-12 08:58:51 +0200

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import axios from 'axios'
 
 import { AuthProvider } from './context/AuthContext'
 import Navigation from './components/Navigation'
@@ -13,49 +12,25 @@ import Account from './screens/Account'
 import Login from './screens/auth/Login'
 import Registration from './screens/auth/Registration'
 import RegistrationVerification from './screens/auth/RegistrationVerification'
+import Course from './components/Course'
 import LessonsList from './screens/LessonsList'
-
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Lesson from './components/Lesson'
 
 function App() {
-  // const [loading, setLoading] = React.useState(false)
-  // const [token, setToken] = React.useState('')
-  // const [isAuthenticated, setIsAuthenticated] = React.useState(false)
-
-  // React.useEffect(() => {
-  //   checkAuthentication()
-  // }, [])
-
-  // const checkAuthentication = async () => {
-  //   try {
-  //     await axios.get(`${process.env.REACT_APP_API_URL}/authentication/check_authentication`)
-  //       .then(res => {
-  //         if (res.data.error) console.log(res.data.error)
-  //         else {
-  //           console.log(res.data.isAuthenticated)
-  //           setIsAuthenticated(res.data.isAuthenticated)
-  //           setLoading(false)
-  //         }
-  //       })
-  //   } catch (err) { console.log(err) }
-  // }
-
-  // if (loading) return <h1>Loading...</h1>
-
   return (
     <BrowserRouter>
       <AuthProvider>
         <Navigation />
         <Routes>
           <Route path='/' exact element={<PrivateRoute><HomePage /></PrivateRoute>} />
-          <Route path=':courseID' element={<PrivateRoute><LessonsList /></PrivateRoute>} />
+          <Route path='course/:courseID' element={<PrivateRoute><Course /></PrivateRoute>} />
+          <Route path='lesson/:lessonID' element={<PrivateRoute><Lesson /></PrivateRoute>} />
           <Route path='login' element={<Login />} />
           <Route path='registration' element={<Registration />} />
           <Route path='account' element={<PrivateRoute><Account /></PrivateRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
-
   )
 }
 
