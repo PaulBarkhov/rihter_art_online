b57eeafec848da275e9bc2dbb848eeed331bca67
paulbarkhov <pavelbarhov@gmail.com>
2022-04-12 09:00:38 +0200

    minor changes

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -8,15 +8,15 @@ import Container from 'react-bootstrap/Container'
 const Navigation = () => {
     const { user, logout } = useContext(AuthContext)
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className="mb-4">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">Rihter Art Online</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link className="nav-link" to="/">Home</Link>
-                        {user && <Link className="nav-link" to="login" onClick={logout}>Logout</Link>}
-                        {!user && <Link className="nav-link" to="login">Login</Link>}
+                        <Link className="nav-link" to="/">Курсы</Link>
+                        {user && <Link className="nav-link" to="login" onClick={logout}>Выйти</Link>}
+                        {!user && <Link className="nav-link" to="login">Войти</Link>}
 
                     </Nav>
                 </Navbar.Collapse>
