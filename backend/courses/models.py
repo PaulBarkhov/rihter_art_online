3e8221e92e9f8a932b1d202c0b29f4346e5786ab
paulbarkhov <pavelbarhov@gmail.com>
2022-04-25 11:29:01 +0200

    added video model and modified photo model

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -8,9 +8,10 @@ class Course(models.Model):
     name = models.CharField('Название', max_length=64, blank=True)
     description = models.TextField('Описание', max_length=512, null=True, blank=True)
     preview = models.ImageField('Фото', upload_to='uploads/previews', null=True, blank=True)
+    draft = models.BooleanField('Черновик', default=False)
 
-    def get_free_lessons(self):
-        return self.lessons_set.filter(access='free')
+    def get_min_price(self):
+        return self.lessonPacks.all().aggregate(models.Min('price'))['price__min']
 
     def __str__(self):
         return f"{self.id}: {self.name}"
@@ -20,29 +21,27 @@ class LessonPack(models.Model):
         verbose_name = 'Группа уроков'
         verbose_name_plural = 'Группы уроков'
     name = models.CharField('Название', max_length=64, blank=True)
-    course = models.ForeignKey(Course, verbose_name='Описание', null=True, on_delete=models.CASCADE, blank=True, related_name='lessonPacks')
+    course = models.ForeignKey(Course, verbose_name='Курс', null=True, on_delete=models.CASCADE, blank=True, related_name='lessonPacks')
     price = models.DecimalField('Цена', max_digits=6, decimal_places=2, null=True, blank=True)
+
 
     def __str__(self):
         return f"{self.course} - {self.name}"
-
 
 class Lesson(models.Model):
     class Meta: 
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
     course = models.ForeignKey(Course, verbose_name='Курс', null=True, on_delete=models.CASCADE, blank=True, related_name='lessons')
-    lessonPack = models.ForeignKey(LessonPack, verbose_name='Уроки', null=True, on_delete=models.CASCADE, blank=True, related_name='lessons')
+    lessonPack = models.ForeignKey(LessonPack, verbose_name='Группа уроков', null=True, on_delete=models.CASCADE, blank=True, related_name='lessons')
     name = models.CharField('Название', max_length=64, blank=True)
     description = models.TextField('Описание', null=True, max_length=512, blank=True)
     preview = models.ImageField('Фото', upload_to='uploads/lessons/previews', null=True, blank=True)
-    video = models.CharField('Видео(url)', max_length=512, null=True, blank=True)
+   
     excersize = models.CharField('Задание', null=True, max_length=512, blank=True)
     access = models.CharField('Доступ', null=True, max_length=4, choices=[('free', 'бесплатно'), ('paid', 'платно')], default='free')
-
     def __str__(self):
         return f"{self.id}: {self.name}, {self.lessonPack}"
-
 
 class Comment(models.Model):
     class Meta: 
@@ -60,12 +59,20 @@ class Photo(models.Model):
     class Meta: 
         verbose_name = 'Фотография'
         verbose_name_plural = 'Фотографии'
-    lesson = models.ForeignKey(Lesson, verbose_name='Урок', null=True, on_delete=models.CASCADE, blank=True, related_name='photos')
+    lesson = models.ForeignKey(Lesson, verbose_name='Урок', null=True, on_delete=models.SET_NULL, blank=True, related_name='photos')
     name = models.CharField('Название', max_length=64, blank=True)
     url = models.ImageField('URL', null=True, upload_to='uploads/photos', blank=True)
 
     def __str__(self):
         return f"{self.id}: {self.name}"
+
+class Video(models.Model):
+    class Meta:
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Видео'
+    name = models.CharField('Название', max_length=64)
+    url = models.TextField('URL')
+    lesson = models.ForeignKey(Lesson, verbose_name='Урок', null=True, on_delete=models.SET_NULL, blank=True, related_name='videos')
 
 class Order(models.Model):
     class Meta: 
@@ -75,3 +82,17 @@ class Order(models.Model):
     course = models.ForeignKey(Course, verbose_name='Курс', on_delete=models.CASCADE, related_name="orders")
     number_of_lessons = models.IntegerField('Количество уроков', )
     status = models.CharField('Статус', max_length=64)
+
+class RatingStar(models.Model):
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+    value = models.SmallIntegerField('Значение', default=0)
+
+class Rating(models.Model):
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+    ip = models.CharField('IP', max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Звезда', related_name='Rating')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс', related_name='Rating')
