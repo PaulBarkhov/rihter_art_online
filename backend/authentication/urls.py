6ee8011d88a354f75ffd06c9ad0ef1e87a93eab3
paulbarkhov <pavelbarhov@gmail.com>
2022-04-25 11:26:59 +0200

    added url for account activation

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('token', views.MyTokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
     path('register', views.RegisterView.as_view()),
+    path('request_verification_code', views.Send_verification_code.as_view()),
     # path('get_csrf', views.GetCSRFToken.as_view()),
     # path('login', views.LoginView.as_view()),
     # path('logout', views.LogoutView.as_view()),
