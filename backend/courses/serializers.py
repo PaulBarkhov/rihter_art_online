896e9834484030b7f89a0aa1e64d109b0eb4e69b
paulbarkhov <pavelbarhov@gmail.com>
2022-04-25 11:29:34 +0200

    minor changes

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,36 +1,53 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SlugRelatedField, SerializerMethodField, FloatField
 from django.contrib.auth.models import User
 from . import models
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('__all__')
 
-class LessonSerializer(serializers.ModelSerializer):
+class PhotoSerializer(ModelSerializer):
+    class Meta:
+        model = models.Photo
+        fields = ('__all__')
+
+class VideoSerializer(ModelSerializer):
+    class Meta:
+        model = models.Video
+        fields = ('__all__')
+
+class LessonListSerializer(ModelSerializer):
+    class Meta:
+        model = models.Lesson
+        fields = ['id', 'name', 'description', 'preview', 'access']
+
+class LessonSerializer(ModelSerializer):
+    photos = PhotoSerializer(read_only=True, many=True)
     class Meta:
         model = models.Lesson
         fields = ('__all__')
 
-class LessonGroupSerializer(serializers.ModelSerializer): 
-    lessons = LessonSerializer(many=True, read_only=True)
+class LessonGroupSerializer(ModelSerializer):
     class Meta:
         model = models.LessonPack
         fields = ('__all__')
 
-
-class CourseListSerializer(serializers.ModelSerializer):
+class CourseListSerializer(ModelSerializer):
     # user = UserSerializer(required=False, read_only=True)
     # profile = ProfileSerializer(read_only=True)
     # lessons = LessonSerializer(read_only=True, many=True)
-    # lessons = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    # lessons_groups = LessonGroupSerializer(read_only=True, many=True)
+    # lessonPacks = LessonGroupSerializer(read_only=True, many=True)
+    min_price = FloatField(source='get_min_price')
+    class Meta:
+        model = models.Course
+        fields = ('__all__') 
+
+class CourseDetailSerializer(ModelSerializer):
+    lessons = LessonListSerializer(read_only=True, many=True)
+    # lessonPacks = LessonGroupSerializer(read_only=True, many=True)
     class Meta:
         model = models.Course
         fields = ('__all__')
-
-class CourseDetailSerializer(serializers.ModelSerializer):
-    lessonPacks = LessonGroupSerializer(read_only=True, many=True)
-    class Meta:
-        model = models.Course
-        fields = ['id', 'name', 'lessonPacks']
