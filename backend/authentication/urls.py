9bddd6320ca573a3d72ff72d93af09b39b377caa
paulbarkhov <pavelbarhov@gmail.com>
2022-05-04 10:36:33 +0200

    most urls were commented because switched to djoser

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -6,12 +6,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
 urlpatterns = [
     path('token', views.MyTokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
-    path('register', views.RegisterView.as_view()),
-    path('request_verification_code', views.Send_verification_code.as_view()),
+    # path('register', views.RegisterView.as_view()),
+    # path('request_verification_code', views.Send_verification_code.as_view()),
     # path('get_csrf', views.GetCSRFToken.as_view()),
     # path('login', views.LoginView.as_view()),
     # path('logout', views.LogoutView.as_view()),
