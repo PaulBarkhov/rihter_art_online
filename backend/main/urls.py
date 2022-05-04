4c4519de22bef6072a05e3db8fc5a3f97de006a7
paulbarkhov <pavelbarhov@gmail.com>
2022-05-04 10:38:18 +0200

    added djoser.urls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("", include('courses.urls')),
     path('authentication/', include('authentication.urls')),
     path('profile/', include('user_profile.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt'))
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
