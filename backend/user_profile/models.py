ae0fe8cc3b86c00dc00dc22e0786f4699395d438
paulbarkhov <pavelbarhov@gmail.com>
2022-05-04 10:41:38 +0200

    changed fields in UserProfile model: language => language_code, status => status_text

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -7,11 +7,11 @@ class UserProfile(models.Model):
     class Meta: 
         verbose_name = 'Профиль'
         verbose_name_plural = 'Профили'
-    user = models.OneToOneField(User, verbose_name='Пользователь', on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, verbose_name='Пользователь', null=True, blank=True, on_delete=models.CASCADE, related_name='profile')
     profile_image = models.ImageField('Аватар', default='default.jpg', upload_to='profile_pics', null=True, blank=True)
     birth_date = models.DateField('Дата рождения', null=True, blank=True, default=datetime.date.today)
-    language = models.TextField('Язык', max_length=512, null=True, blank=True)
-    status = models.TextField('Статус', max_length=512, null=True, blank=True)
+    language_code = models.TextField('Язык', max_length=512, null=True, blank=True)
+    status_text = models.TextField('Статус', max_length=512, null=True, blank=True)
 
     purchased_lessonPacks = models.ManyToManyField(LessonPack, verbose_name='Купленные уроки', blank=True, related_name='profiles')
 
@@ -21,5 +21,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.user}"
-
-        # 202 + 90 + 80 + 
