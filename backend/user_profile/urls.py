7090339a50f3aae396ce20d5f6fe696ab88c0920
paulbarkhov <pavelbarhov@gmail.com>
2022-05-20 08:23:35 +0200

    minor changes

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from .views import UpdateProfileImage, UpdateProfileView, UserProfileView
 
-
 urlpatterns = [
     path('me', UserProfileView.as_view()),
     path('update', UpdateProfileView.as_view()),
