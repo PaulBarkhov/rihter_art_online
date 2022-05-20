b24f1f0fa36f7b623be132b50b4ed45dab7bde6b
paulbarkhov <pavelbarhov@gmail.com>
2022-05-20 08:00:31 +0200

    made profile image a bit smaller

--- frontend/src/screens/Profile.js
+++ frontend/src/screens/Profile.js
@@ -55,7 +55,7 @@ function Account() {
         <div className="d-flex justify-content-center">
             <div className="col-md-7 col-12 border rounded shadow p-4">
                 <div className="d-flex flex-column w-100 align-items-center justify-content-center mb-2">
-                    <img src={profileData.profile_image} alt="Profile" style={{ width: 200, height: 200, borderRadius: 100, marginBottom: 10, objectFit: 'cover' }} />
+                    <img src={profileData.profile_image} alt="Profile" style={{ width: 150, height: 150, borderRadius: 100, marginBottom: 10, objectFit: 'cover' }} />
                     <h2>{profileData.first_name} {profileData.last_name}</h2>
                     <h6>{profileData.username}</h6>
                 </div>
