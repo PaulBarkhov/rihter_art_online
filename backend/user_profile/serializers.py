31506d5afcf1c184202165edcba594bf890be659
paulbarkhov <pavelbarhov@gmail.com>
2022-05-20 08:23:11 +0200

    changed fields in UserProfileSerializer

--- backend/user_profile/serializers.py
+++ backend/user_profile/serializers.py
@@ -4,4 +4,4 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = ('id', 'birth_date', 'phone_number', 'sex', 'about_self', 'profile_image')
