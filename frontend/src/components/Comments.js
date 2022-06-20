371fd64dbbc79462f5e992bdd623ae8f9f3b318c
paulbarkhov <pavelbarhov@gmail.com>
2022-06-20 17:34:48 +0200

    implemented comments deleting and moved input to separate component ChatInput.js

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,33 +1,21 @@
-import React, { useEffect, useState, useContext, useRef } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
-import { useParams, useNavigate, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import Spinner from 'react-bootstrap/Spinner'
-import autosize from 'autosize';
-import { ArrowRight, X } from 'react-bootstrap-icons'
-import VoiceRecorder from './voiceRecorder/VoiceRecorder'
-// import AudioPlayer from './AudioPlayer'
 
 import AudioPlayer from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
 
+import ChatInput from './ChatInput'
+
 const Comments = () => {
     const params = useParams()
-    const navigate = useNavigate()
-    const { tokens, logout, setHeader } = useContext(AuthContext)
+    const { tokens, logout, user } = useContext(AuthContext)
     const [loading, setLoading] = useState(true)
     const [reloading, setReloading] = useState(false)
     const [comments, setComments] = useState('')
-
-    const [commentText, setCommentText] = useState('')
-    const [recording, setRecording] = useState(false)
-    const [voice, setVoice] = useState()
     const [reply, setReply] = useState()
-
-    const textareaRef = useRef()
-    const audioRef = useRef()
-
-    useEffect(() => { autosize(textareaRef.current) }, [])
 
     useEffect(() => {
         const fetchComments = async () => {
@@ -36,7 +24,7 @@ const Comments = () => {
                     'Authorization': `JWT ${tokens.access}`
                 }
             }
-            await axios.get(`${process.env.REACT_APP_API_URL}/lesson/${params.lessonID}/comments`, config)
+            await axios.get(`${process.env.REACT_APP_API_URL}/api/lesson/${params.lessonID}/comments`, config)
                 .then(res => {
                     setComments(res.data.comments)
                     setLoading(false)
@@ -46,7 +34,7 @@ const Comments = () => {
         fetchComments()
     }, [params.lessonID, tokens, reloading])
 
-    const sendComment = async () => {
+    const sendComment = async (commentText, voice) => {
         if (commentText || voice) {
             const config = {
                 headers: {
@@ -59,21 +47,33 @@ const Comments = () => {
             formData.append("voice", voice ? voice.blob : '')
             formData.append("parentID", reply ? reply.id : '')
 
-            await axios.post(`${process.env.REACT_APP_API_URL}/lesson/${params.lessonID}/comments`, formData, config)
+            await axios.post(`${process.env.REACT_APP_API_URL}/api/lesson/${params.lessonID}/comments`, formData, config)
                 .then(res => setReloading(!reloading))
                 .catch(err => err.response.status === 401 ? logout() : console.log(err.response))
-                .finally(() => {
-                    setCommentText('')
-                    setVoice(null)
-                    setReply(null)
-                })
         }
     }
+
+    const deleteComment = async (id) => {
+        const config = {
+            headers: {
+                'Authorization': `JWT ${tokens.access}`,
+                "Content-Type": "multipart/form-data"
+            }
+        }
+
+        await axios.delete(`${process.env.REACT_APP_API_URL}/api/comment/${id}`, config)
+            .then(res => {
+                console.log(res)
+                setReloading(!reloading)
+            })
+            .catch(err => err.response.status === 401 ? logout() : console.log(err.response))
+    }
+
 
     if (loading) return <Spinner animation='border' className="spinner-border-xl f-flex justify-" />
 
     return (
-        <div className="position-relative" style={{ paddingBottom: textareaRef.current && textareaRef.current.style.height }}>
+        <div className="position-relative" style={{ paddingBottom: 70 }}>
             {comments.length === 0 ? <h1>Пока нет комментариев</h1> : comments.map(comment => {
                 return (
                     <div key={comment.id} className="border rounded p-3 my-3">
@@ -95,15 +95,24 @@ const Comments = () => {
                             customVolumeControls={[]}
                         />
                         }
-                        {/* {comment.voice && <AudioPlayer voice={comment.voice} />} */}
-                        <button
-                            className="btn btn-link"
-                            onClick={() => {
-                                setReply(comment)
-                                textareaRef.current.focus()
-                            }}>
-                            Ответить
-                        </button>
+                        {
+                            comment.user.id === user.user_id ? (
+                                <button
+                                    className="btn btn-link"
+                                    style={{ color: 'red' }}
+                                    onClick={() => deleteComment(comment.id)}>
+                                    Удалить комментарий
+                                </button>
+
+                            ) : (
+                                <button
+                                    className="btn btn-link"
+                                    onClick={() => setReply(comment)}>
+                                    Ответить
+                                </button>
+                            )
+                        }
+
                         <div style={{ borderLeft: "1px solid grey" }}>
                             {comment.children.map(child => <div key={child.id} className="p-2">
                                 <div className="d-flex flex-row">
@@ -130,52 +139,7 @@ const Comments = () => {
                     </div>
                 )
             })}
-
-            <div className="p-2 fixed-bottom bg-white" >
-                <div className="d-flex flex-row ">
-                    {voice ? (
-                        <div className='w-100'>
-                            <AudioPlayer
-                                src={voice.audio}
-                                layout="horizontal-reverse"
-                                showJumpControls={false}
-                                autoPlayAfterSrcChange={false}
-                                showDownloadProgress={false}
-                                customAdditionalControls={[]}
-                                customVolumeControls={[]}
-                            />
-
-                        </div>
-                    ) : (
-                        <div className="w-100">
-                            <textarea
-                                ref={textareaRef}
-                                rows="1"
-                                className="form-control"
-                                style={{ maxHeight: 150, resizeL: "none" }}
-                                name="commentTextArea"
-                                id="commentTextArea"
-                                disabled={recording}
-                                onChange={e => setCommentText(e.target.value)}
-                                value={commentText}
-                            />
-                            <div className="position-absolute bg-white" style={{ top: -15, left: "1rem" }}>
-                                <span>{reply ? `${reply.user.first_name} ${reply.user.last_name},` : "Ваш комментарий:"}</span>
-                                {reply && <X color="red" size="20" onClick={() => setReply('')} />}
-                            </div>
-                        </div>
-                    )}
-
-                    {commentText || voice ? (
-                        <div className="fixed-right d-flex flex-row">
-                            {voice && <button className="btn btn-danger mx-1" onClick={() => setVoice(null)}><X size="20" /></button>}
-                            <button className="btn btn-primary" onClick={() => sendComment()}><ArrowRight size="20" /></button>
-                        </div>
-                    ) : (
-                        <VoiceRecorder setVoice={setVoice} setRecording={setRecording} />
-                    )}
-                </div>
-            </div>
+            <ChatInput send={sendComment} reply={reply} setReply={setReply} />
         </div >
     )
 }
